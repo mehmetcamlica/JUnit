7ab2d2a2f71fdeef027b8dd2515f30a0b1819744
mehmetcamlica <mehmetcamlica20@gmail.com>
2024-03-25 18:12:16 +0100

    first

---
+++ src/test/java/Utilities/ReusableMethods.java
@@ -0,0 +1,33 @@
+package utilities;
+
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReusableMethods {
+
+    public static void bekle(int saniye){
+
+        try {
+            Thread.sleep(saniye * 1000);
+        } catch (InterruptedException e) {
+            System.out.println("Bekleme isleminde sorun olustu");
+        }
+
+    }
+
+    public static List<String> stringListeDonustur(List<WebElement> webElementList){
+
+        List<String> donusturulenList = new ArrayList<>();
+
+        for (WebElement eachElement : webElementList
+        ) {
+
+            donusturulenList.add(eachElement.getText());
+        }
+
+
+        return donusturulenList;
+    }
+}
---
+++ src/test/java/day05_assertions_dropdownMenu/C04_RadioButtons.java
@@ -0,0 +1,68 @@
+package day05_assertions_dropdownMenu;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.TestBase_BeforeAfterClass;
+
+import java.time.Duration;
+
+public class C04_RadioButtons extends TestBase_BeforeAfterClass {
+
+    //Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın.
+    //	a. Verilen web sayfasına gidin.
+    //	      https://testotomasyonu.com/form
+    //	b. Cinsiyet Radio button elementlerini locate edin
+    //	c. Iki farkli test method’u oluşturup yazidan veya direk buton’dan size uygun olani secin
+    //	d. Sectiginiz radio button’un seçili, ötekilerin seçili olmadigini test edin
+
+
+    @Test
+    public void yazidanSecimTesti(){
+
+        driver.get("https://testotomasyonu.com/form");
+
+        WebElement kadinYaziElementi = driver.findElement(By.xpath("//*[@for ='inlineRadio1']"));
+        WebElement erkekYaziElementi = driver.findElement(By.xpath("//*[@for ='inlineRadio2']"));
+        WebElement digerYaziElementi = driver.findElement(By.xpath("//*[@for ='inlineRadio3']"));
+
+        erkekYaziElementi.click();
+
+        WebElement kadinCheckbox = driver.findElement(By.xpath("//*[@id ='inlineRadio1']"));
+        WebElement erkekCheckbox = driver.findElement(By.xpath("//*[@id ='inlineRadio2']"));
+        WebElement digerCheckbox = driver.findElement(By.xpath("//*[@id ='inlineRadio3']"));
+
+        // erkek'in secili oldugunu test edelim
+        Assert.assertTrue(erkekCheckbox.isSelected());
+
+        // kadin ve diger'in secili olmadigini test edelim
+        Assert.assertFalse(kadinCheckbox.isSelected());
+        Assert.assertFalse(digerCheckbox.isSelected());
+
+    }
+
+    @Test
+    public void checkboxSecimTesti(){
+
+        driver.get("https://testotomasyonu.com/form");
+
+        WebElement kadinCheckbox = driver.findElement(By.xpath("//*[@id ='inlineRadio1']"));
+        WebElement erkekCheckbox = driver.findElement(By.xpath("//*[@id ='inlineRadio2']"));
+        WebElement digerCheckbox = driver.findElement(By.xpath("//*[@id ='inlineRadio3']"));
+
+        erkekCheckbox.click();
+
+        // erkek'in secili oldugunu test edelim
+        Assert.assertTrue(erkekCheckbox.isSelected());
+
+        // kadin ve diger'in secili olmadigini test edelim
+        Assert.assertFalse(kadinCheckbox.isSelected());
+        Assert.assertFalse(digerCheckbox.isSelected());
+    }
+}
